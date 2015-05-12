26591200002516916b7d04486522b88ced2278cf
Arnaud Bertrand <arnaud.bertrand.15@eigsi.fr>
2015-05-12 15:58:39 +0100

    server update

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var Hapi = require('hapi');
 
 // Create a server with a host and port
-var server = new Hapi.Server();
+var server = new Hapi.Server(+process.env.PORT, '0.0.0.0');
 server.connection({ 
-  host: 'localhost', 
-  port: 8000,
   routes: {cors: true}
 });
 
