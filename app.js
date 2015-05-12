029b21a9b863d48e38e7891f47db31e12f93f4ff
Arnaud Bertrand <arnaud.bertrand.15@eigsi.fr>
2015-05-12 16:13:53 +0100

    test launch server

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 var Hapi = require('hapi');
 
 // Create a server with a host and port
-var server = new Hapi.Server(+process.env.PORT, '0.0.0.0');
+var server = new Hapi.Server();
 server.connection({ 
+  host: '0.0.0.0', 
+  port: +process.env.PORT,
   routes: {cors: true}
 });
+
+server.route({
+  method: 'GET',
+  path:'/hello', 
+  handler: function (request, reply) {
+    reply('hello world');
+    console.log('test');
+  }
+});
+
+// Start the server
+server.start();
 
 // Add the route
 server.route(
