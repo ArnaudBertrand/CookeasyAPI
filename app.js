7be5e33f23d9b755a119a1873c557d540bcb2889
Arnaud Bertrand <arnaud.bertrand.15@eigsi.fr>
2015-05-12 16:04:58 +0100

    new route for testing

--- app.js
+++ app.js
@@ -7,14 +7,22 @@ server.connection({
 });
 
 // Add the route
-server.route({
-  method: 'GET',
-  path:'/hello', 
-  handler: function (request, reply) {
-    reply('hello world');
-    console.log('test');
-  }
-});
+server.route(
+  {
+    method: 'GET',
+    path:'/', 
+    handler: function (request, reply) {
+      reply('test');
+    }
+  },
+  {
+    method: 'GET',
+    path:'/hello', 
+    handler: function (request, reply) {
+      reply('hello world');
+      console.log('test');
+    }
+  });
 
 // Start the server
 server.start();
