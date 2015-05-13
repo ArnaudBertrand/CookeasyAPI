f50da0b52288712ca3681a3a35f7d2b5b0521d6f
Arnaud Bertrand <arnaud.bertrand.15@eigsi.fr>
2015-05-13 17:10:54 +0100

    test authentication

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var Hapi = require('hapi');
+var Bcrypt = require('bcrypt');
+var Basic = require('hapi-auth-basic');
 var Joi = require('joi');
 var Mongoose = require('mongoose');
 
-var internals = {};
+var handlers = {};
 var configs = {};
 
 // Create a server with a host and port
@@ -13,37 +15,61 @@ server.connection({
   routes: {cors: true}
 });
 
-internals.get = function (request, reply) {
+// Routes handlers
+handlers.get = function (request, reply) {
   reply('Home page');
 };
 
-internals.contact = function (request, reply) {
+handlers.contact = function (request, reply) {
   reply('Contact page');
   console.log('test');
 };
 
-internals.userLogin = function(request, reply){
+handlers.userLogin = function(request, reply){
   console.log(request.payload);
   console.log(request.payload.password);
   console.log(request.payload["password"]);
   reply(request.payload.password + ' ' + request.payload.username);
-}
+};
 
+handlers.test = function(request, reply){
+  console.log(request.payload);
+  console.log('test ok')
+}
 configs.userLogin = {
   validate: { payload: {
   	  username: Joi.string().min(3),
       password: Joi.string().min(3)
   }}};
 
-// Add the route
+// Routes definitions
 server.route([
-  { method: 'GET', path:'/', handler: internals.get },
-  { method: 'POST', path:'/user/login', handler: internals.userLogin, config: configs.userLogin},
-  { method: 'GET', path:'/hello', handler: internals.contact }
+  { method: 'GET', path:'/', handler: handlers.get },
+  { method: 'POST', path:'/user/login', handler: handlers.userLogin, config: configs.userLogin},
+  { method: 'GET', path:'/hello', handler: handlers.contact }
 ]);
 
 // Start the server
 server.start(function(){ console.log('Server started at [' + server.info.uri + ']'); });
+
+// Validation
+var validate = function(username,password,callback){
+	// Find usernname
+	var user = User.find({});
+	if(!user){
+		return callback(null,false);
+	}
+
+	Bcrypt.compare(password,user.password, function(err, isValid){
+	  callback(err, isValid, {id: user.id, username: user.username});
+	});
+};
+
+// Authentication
+server.register(require('hapi-auth-basic'), function (err) {
+    server.auth.strategy('simple', 'basic', { validateFunc: validate });
+    server.route({ method: 'GET', path: '/test', handler: handlers.test config: { auth: 'simple' } });
+});
 
 // Connect to database
 Mongoose.connect('mongodb://cookeasyapi:apiPASSWORD@ds061731.mongolab.com:61731/heroku_app36784651');
@@ -54,11 +80,7 @@ db.once('open', function callback() {
 });
 
 var userSchema = Mongoose.Schema({
-  name: String,
-  password: String
+  username: {type: String, required: true, unique: true},
+  password: {type: String, required: true}
 });
 var User = Mongoose.model('users',userSchema);
-User.find(function(err, users){
-  if(err) return console.log(err);
-  console.log(users);
-});
