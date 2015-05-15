47a3cd49956bae1a5791be3076fd18969bebbdec
Arnaud Bertrand <arnaud.bertrand.15@eigsi.fr>
2015-05-15 14:54:03 +0100

    test authentication

--- mongoose/user.js
+++ mongoose/user.js
@@ -11,27 +11,19 @@ var UserSchema = new Schema({
 UserSchema.pre('save', function(next) {
   var user = this;
 
-  // only hash the password if it has been modified (or is new)
+  // Only hash the password if it has been modified (or is new)
   if (!user.isModified('password')){
     return next();
   }
 
-  // generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+  // Hash the password
+  bcrypt.hash(user.password, SALT_WORK_FACTOR, function(err, hash) {
     if (err){
       return next(err);
     }
-
-    // hash the password using our new salt
-    bcrypt.hash(user.password, salt, function(err, hash) {
-      if (err){
-        return next(err);
-      }
-
-      // override the cleartext password with the hashed one
-      user.password = hash;
-      next();
-    });
+    // Override the cleartext password with the hashed one
+    user.password = hash;
+    next();
   });
 });
 
