1b93a6a6835ab0e532ec03ca2d91cec64969ef21
Arnaud Bertrand <arnaud.bertrand.15@eigsi.fr>
2015-05-18 11:50:30 +0100

    test express

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 // Set up
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
