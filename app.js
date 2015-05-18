269793fb6fd342af26a489d003f369914fd1f7eb
Arnaud Bertrand <arnaud.bertrand.15@eigsi.fr>
2015-05-18 11:53:26 +0100

    test express

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var port = process.env.PORT || 3000;
 // Set up
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 app.use(morgan('dev'));
