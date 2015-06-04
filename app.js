62091633c316709b13114a2054a1cb208a9c9515
Arnaud Bertrand <arnaud.bertrand.15@eigsi.fr>
2015-06-04 17:51:10 +0100

    url picture upload

--- app.js
+++ app.js
@@ -42,7 +42,8 @@ router.post('/user/signup', internals.user.signup);
 router.post('/recipe/create', jwt({secret: secret.secretToken}), tokenManager.verifyToken, internals.recipe.create);
 router.post('/recipe/comment/add/:id', jwt({secret: secret.secretToken}), tokenManager.verifyToken, internals.recipe.addComment);
 router.delete('/recipe/delete/:id', jwt({secret: secret.secretToken}), tokenManager.verifyToken, internals.recipe.delete);
-router.post('/recipe/pictures/upload/:id', jwt({secret: secret.secretToken}), tokenManager.verifyToken, multipartyMiddleware, internals.recipe.uploadPictures);
+//router.post('/recipe/pictures/upload/:id', jwt({secret: secret.secretToken}), tokenManager.verifyToken, multipartyMiddleware, internals.recipe.uploadPictures);
+router.post('/recipe/pictures/upload/:id', multipartyMiddleware, internals.recipe.uploadPictures);
 router.post('/recipe/step/picture/upload', jwt({secret: secret.secretToken}), tokenManager.verifyToken, multipartyMiddleware, internals.recipe.uploadStepPicture);
 
 app.use('/',router);
