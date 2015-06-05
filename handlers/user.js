ebdc8dfb8403f76ca0847f800b636d2d9c446bf0
Arnaud Bertrand <arnaud.bertrand.15@eigsi.fr>
2015-06-05 17:43:06 +0100

    test login

--- handlers/user.js
+++ handlers/user.js
@@ -7,13 +7,13 @@ var internals = {};
 internals.login = function(req, res){
   // ID
   var id = req.body.id || '';
-  console.log(id);
+  console.log('ID ' + id);
   if(typeof id !== "string"){
     return res.send({error: 'Uncorrect identifier'},400);
   }
   var isEmail = id === id.match(/[a-z0-9]*@[a-z0-9]*\.[a-z]*/i);
   // Password
-  console.log(password);
+  console.log('PWD' + password);
   var password = req.body.password || '';
   if(typeof password !== "string" || password.length < 6){
     return res.send({error: 'Password too short'},400);
