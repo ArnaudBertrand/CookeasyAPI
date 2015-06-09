3940417230f0bccf2bbce801f192ad962044b73e
Arnaud Bertrand <arnaud.bertrand.15@eigsi.fr>
2015-06-09 09:48:09 +0100

    print req

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(function(req,res,next){
-  console.log(req);
+  console.log(req.headers);
+  console.log(req.authorization);
   next();
 });
 
