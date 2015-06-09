22da6078a4df7d28d721a2770752649ee335bb42
Arnaud Bertrand <arnaud.bertrand.15@eigsi.fr>
2015-06-09 09:48:20 +0100

    print req

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(function(req,res,next){
   console.log(req.headers);
-  console.log(req.authorization);
+  console.log(req.headers.authorization);
   next();
 });
 
