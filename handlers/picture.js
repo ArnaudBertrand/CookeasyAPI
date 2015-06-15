c999870e87384671a5d2fd9ddc69034a439166e0
Arnaud Bertrand <arnaud.bertrand.15@eigsi.fr>
2015-06-15 10:52:37 +0100

    pictures formats

--- handlers/picture.js
+++ handlers/picture.js
@@ -16,8 +16,14 @@ internals.upload = function(req,res){
       // Set picture and thumbnail
       var picture = {};
       picture.url = result.url;
-      picture.miniThumbUrl = cloudinary.url(result.public_id, { width: 100, height: 100, crop: "fill" });
-      picture.thumbUrl = cloudinary.url(result.public_id, { width: 300, height: 300, crop: "fill" });
+      // Mini
+      picture.miniThumbUrlLarge = cloudinary.url(result.public_id, { width: 180, height: 120, crop: "fill" });
+      picture.miniThumbUrlSquare = cloudinary.url(result.public_id, { width: 100, height: 100, crop: "fill" });
+      picture.miniThumbUrlLong = cloudinary.url(result.public_id, { width: 120, height: 180, crop: "fill" });
+      // Thumb
+      picture.thumbUrlLarge = cloudinary.url(result.public_id, { width: 300, height: 200, crop: "fill" });
+      picture.thumbUrlSquare = cloudinary.url(result.public_id, { width: 300, height: 300, crop: "fill" });
+      picture.thumbUrlLong = cloudinary.url(result.public_id, { width: 200, height: 300, crop: "fill" });
       picture.author = author;
       picture.createdOn =  Date.now();
 
