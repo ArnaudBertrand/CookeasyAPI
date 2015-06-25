8631cfcb380136c2ea02c7e7c017985fa8280da6
Arnaud Bertrand <arnaud.bertrand.15@eigsi.fr>
2015-06-25 22:54:05 +0100

    user dao creation

--- dao/user.dao.js
+++ dao/user.dao.js
@@ -1,20 +1,22 @@
 var User = require('./../mongoose/user.js');
 
-function UserDao() {
-  this.login = function (query,callback){
-    User.findOne(query,function(err,user){
-      if(err) return callback(err,null,null);
-      if(!user) return callback(null,false,{userId: "User not found"});
+var UserDao = {
+  login: login
+};
 
-      user.comparePassword(password, function(err, isValid){
-        if(err) return callback(null,false,null);
-        if(!isValid) return callback(null,false,{password: "Wrong password"});
+function login (query,callback){
+  User.findOne(query,function(err,user){
+    if(err) return callback(err,null,null);
+    if(!user) return callback(null,false,{userId: "User not found"});
 
-        callback(null,true,user);
-      });
+    user.comparePassword(password, function(err, isValid){
+      if(err) return callback(null,false,null);
+      if(!isValid) return callback(null,false,{password: "Wrong password"});
 
+      callback(null,true,user);
     });
-  }
+
+  });
 }
 
 module.exports = UserDao;
